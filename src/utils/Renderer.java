f41522f96a4ecbba675bb1657b48ad036838349b
skodami1 <skoda.michael@gmail.com>
2018-10-08 11:39:38 +0200

    DDA line

--- src/utils/Renderer.java
+++ src/utils/Renderer.java
@@ -8,23 +8,23 @@ public class Renderer {
     private BufferedImage img;
 
 
-    public Renderer(BufferedImage img){
+    public Renderer(BufferedImage img) {
         this.img = img;
         color = Color.RED.getRGB();
     }
 
-    private void drawPixel(int x, int y){
+    private void drawPixel(int x, int y) {
         img.setRGB(x, y, color);
     }
 
-    public void lineTrivial(int x1, int y1, int x2, int y2){
+    public void lineTrivial(int x1, int y1, int x2, int y2) {
         // y = k*x+q
         int dx = x1 - x2;
         int dy = y1 - y2;
 
-        if(Math.abs(dx) > Math.abs(dy)){
+        if (Math.abs(dx) > Math.abs(dy)) {
 
-            if(x1 > x2){
+            if (x1 > x2) {
                 int p = x1;
                 x1 = x2;
                 x2 = p;
@@ -34,15 +34,15 @@ public class Renderer {
             }
 
             float k = (float) dy / (float) dx;
-            for (int x = x1; x < x2; x++){
-                int y = y1 + (int)(k* (x - x1));
-                drawPixel(x,y);
+            for (int x = x1; x < x2; x++) {
+                int y = y1 + (int) (k * (x - x1));
+                drawPixel(x, y);
             }
 
 
-        }else {
+        } else {
 
-            if(y1 > y2){
+            if (y1 > y2) {
                 int p = y1;
                 y1 = y2;
                 y2 = p;
@@ -52,32 +52,53 @@ public class Renderer {
             }
 
             float k = (float) dx / (float) dy;
-            for (int y = y1; y < y2; y++){
-                int x = x1 + (int)(k* (y - y1));
-                drawPixel(x,y);
+            for (int y = y1; y < y2; y++) {
+                int x = x1 + (int) (k * (y - y1));
+                drawPixel(x, y);
             }
         }
 
     }
 
-    public void lineDDA(int x1, int y1, int x2, int y2){
-        int dx = x2-x1;
-        int dy = y2-y1;
+    public void lineDDA(int x1, int y1, int x2, int y2) {
+        int dx = x2 - x1;
+        int dy = y2 - y1;
         float x, y, k, g, h;
-        x = x1;
-        y = y1;
-        k = dy/ (float) dx;
 
-        if(dx > dy){
+        k = dy / (float) dx;
+
+        if (Math.abs(dx) > Math.abs(dy)) {
             g = 1;
             h = k;
+
+            if (x1 > x2) {
+                int temp = x1;
+                x1 = x2;
+                x2 = temp;
+                temp = y1;
+                y1 = y2;
+                y2 = temp;
+            }
+
         } else {
-            g = 1/k;
+            g = 1 / k;
             h = 1;
+
+            if (y1 > y2) {
+                int temp = x1;
+                x1 = x2;
+                x2 = temp;
+                temp = y1;
+                y1 = y2;
+                y2 = temp;
+            }
         }
 
-        for(int i=0; i <= Math.max(dx,dy); i++){
-            drawPixel(Math.round(x),Math.round(y));
+        int max = Math.max(Math.abs(dx), Math.abs(dy));
+        x = x1;
+        y = y1;
+        for (int i = 0; i <= max; i++) {
+            drawPixel(Math.round(x), Math.round(y));
             x = x + g;
             y = y + h;
         }
